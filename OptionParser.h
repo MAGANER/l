bd6e7e74309b7c788bb80305226a9d29f50fb8ca
MAGANER <maganer.krik@mail.ru>
2023-08-10 15:23:06 +0300

    structure and function to parse all arguments passed to the programm

---
+++ OptionParser.h
@@ -0,0 +1,106 @@
+#ifndef OPTION_PARSER_H
+#define OPTION_PARSER_H
+#include<string>
+#include<filesystem>
+#include<iostream>
+
+namespace fs = std::filesystem;
+struct Options
+{
+	bool sort;
+	std::string sorting_order; //fd or df
+
+	bool show_only_dirs, show_only_files, show_all;
+	bool show_as_list, show_as_tree, show_as_table; //only one must be true
+	std::string dir; //it's current working directory by default
+
+	inline bool is_regime_showing_ok()
+	{
+		return (show_as_list and !show_as_tree and !show_as_table) or
+			   (!show_as_list and show_as_tree and !show_as_table) or
+			   (!show_as_list and !show_as_tree and show_as_table);
+	}
+	inline bool is_default_output_mode()
+	{
+		//user did not provided any flag, so output will be default
+		return !(show_as_list or show_as_tree or show_as_table);
+	}
+
+	Options() 
+	{
+		dir = fs::current_path().string();
+		show_all = true;
+
+		//l -d -f <=> l 
+		/*
+			so if you want to show directories and files, then just don't type any
+			flag related, but if you want only files, type -f and if you need only
+			directories type -d
+		*/
+		show_only_dirs  = false;
+		show_only_files = false;
+
+		show_as_list	= false;
+		show_as_tree    = false;
+		show_as_table   = false;
+
+		sort = false;
+	}
+	~Options() {}
+};
+
+static inline bool is_option(const std::string& arg)
+{
+	return std::string("-d-f-l-m-t-s").find(arg) != std::string::npos;
+}
+static Options* parse_args(int argc, char** argv)
+{
+	Options* options = new Options();
+	std::string args;
+
+	for (size_t i = 1; i < argc; i++)
+	{
+		auto arg = std::string(argv[i]);
+		if (args.find(arg) == std::string::npos)
+			args += arg;
+		else
+		{
+			std::cout <<"Error:"<< arg << " is already added as flag!" << std::endl;
+		}
+
+		if (fs::is_directory(arg))
+		{
+			options->dir = arg;
+		}
+		else if (not is_option(arg))
+		{
+			std::cout << "Error:" << arg << " is expected to be a directory!" << std::endl;
+			exit(-1);
+		}
+		else if (arg == "-d")
+		{
+			options->show_only_dirs = true;
+			options->show_all = false;
+			options->sorting_order += "d";
+		}
+		else if (arg == "-f")
+		{
+			options->show_only_files = true;
+			options->show_all = false;
+			options->sorting_order += "f";
+		}
+		else if (arg == "-l") options->show_as_list = true;
+		else if (arg == "-m") options->show_as_table = true;//m means matrix
+		else if (arg == "-t") options->show_as_tree = true;
+		else if (arg == "-s")options->sort = true;
+		else
+		{
+			std::cout << "Error:" << arg << " is unknown argument! Try using -h to see help!" << std::endl;
+			exit(-1);
+
+		}
+	}
+	return options;
+}
+
+#endif 
