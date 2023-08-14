3e7772b03aceb21165a405c7a81d964b782c21aa
MAGANER <maganer.krik@mail.ru>
2023-08-14 17:39:31 +0300

    print as tree if required

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include"OptionParser.h"
+#include"Printer.h"
+#include"ConfigReader.h"
+int main(int argc, char** argv)
+{
+	auto options = parse_args(argc, argv);
+	read_config_file(options);
+
+	//check there are no exluding flags
+	//because you can show only as list, table or tree
+	if (!options->is_regime_showing_ok() and !options->is_default_output_mode())
+	{
+		std::cout << "Error: you can use only one flag: -l, -m or -t, because  they exclude each other!" << std::endl;
+		exit(-1);
+	}
+
+	//default mode is equal to list showing mode
+	if (options->is_default_output_mode() or options->show_as_list)
+		Printer::print_as_list(options);
+	else if (options->show_as_table)
+		Printer::print_as_table(options);
+	else if (options->show_as_tree)
+		Printer::print_as_tree(options);
+
+	delete options;
+
+	return 0;
+}
